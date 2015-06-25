ca9697a63dc6c0de8cc0fbd93449cdf09f5394cb
Patrick Snape <patricksnape@gmail.com>
2015-06-25 17:19:59 +0100

    Try making an MKL build
    Doesn't work due to unsatisfiable dependencies??

---
+++ conda/mkl/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+mkdir build
+cd build
+
+CMAKE_ARCH="-m"$ARCH
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  export CFLAGS="$CFLAGS -fPIC $CMAKE_ARCH"
+  export LDFLAGS="$LDFLAGS $CMAKE_ARCH"
+  DYNAMIC_EXT="so"
+  EXTRA_FLAGS=""
+fi
+if [ "$(uname -s)" == "Darwin" ]; then
+  DYNAMIC_EXT="dylib"
+  EXTRA_FLAGS="-DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
+fi
+
+if [ $PY3K -eq 1 ]; then
+  export PY_STR="${PY_VER}m"
+else
+  export PY_STR="${PY_VER}"
+fi
+
+INCLUDE_PATH=${PREFIX}/include
+LIBRARY_PATH=${PREFIX}/lib
+export LDFLAGS="-L${LIBRARY_PATH} $LDFLAGS"
+
+cmake ../tools/python \
+-DCMAKE_PREFIX_PATH=$PREFIX \
+-DBUILD_SHARED_LIBS=1 \
+-DBoost_USE_STATIC_LIBS=0 \
+-DBoost_USE_STATIC_RUNTIME=0 \
+-DBOOST_INCLUDEDIR="${INCLUDE_PATH}" \
+-DBOOST_LIBRARYDIR="${LIBRARY_PATH}" \
+-DPYTHON_LIBRARY="${LIBRARY_PATH}/libpython$PY_STR.$DYNAMIC_EXT" \
+-DPYTHON_INCLUDE_DIR="${INCLUDE_PATH}/python$PY_STR" \
+-DDLIB_NO_GUI_SUPPORT=1 \
+-DDLIB_USE_BLAS=1 \
+-DDLIB_USE_LAPACK=1 ${EXTRA_FLAGS} \
+-DUSE_SSE2_INSTRUCTIONS=1 \
+-DUSE_SSE4_INSTRUCTIONS=1 \
+-DUSE_AVX_INSTRUCTIONS=1
+
+cmake --build . --config Release --target install
+
+cp dlib.so $SP_DIR
+
